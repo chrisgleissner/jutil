3c246774984f5a076720ba117f535dcc20525051
Christian Gleissner <chrisgleissner@gmail.com>
2018-08-15 22:28:23 +0100

    Logging stats

--- src/main/java/uk/gleissner/log/Log.java
+++ src/main/java/uk/gleissner/log/Log.java
@@ -1,27 +1,44 @@
 package uk.gleissner.log;
 
+import com.google.common.math.StatsAccumulator;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.logging.LogFactory;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static java.lang.String.format;
+import static java.lang.System.nanoTime;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 public class Log {
 
     static final int MAX_THREAD_BACKOFF_IN_MS = 10;
+    static final String RANDOM_LOG_MESSAGE = RandomStringUtils.randomAlphabetic(100);
 
     static final Random random = new Random();
+
     static final Logger jul = Logger.getLogger(Log.class.getName());
+    static final StatsAccumulator julStats = new StatsAccumulator();
+
     static final org.apache.log4j.Logger log4j = org.apache.log4j.Logger.getLogger(Log.class);
+    static final StatsAccumulator log4jStats = new StatsAccumulator();
+
     static final org.apache.commons.logging.Log jcl = LogFactory.getLog(Log.class);
+    static final StatsAccumulator jclStats = new StatsAccumulator();
+
     static final org.slf4j.Logger slf4j = LoggerFactory.getLogger(Log.class);
+    static final StatsAccumulator slf4jStats = new StatsAccumulator();
+
+    static ReentrantReadWriteLock statsLock = new ReentrantReadWriteLock(true);
+
 
     public Log(int numberOfThreads, int numberOflogs) {
         SLF4JBridgeHandler.removeHandlersForRootLogger();
@@ -34,19 +51,51 @@ public class Log {
             e = e2;
         }
 
+        ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutor.scheduleAtFixedRate(() -> {
+            try {
+                statsLock.readLock().lock();
+                System.out.println(format("Mean/Max/StdDev:\t\tJUL:%d/%d/%d\tJCL:%d/%d/%d\tLog4J:%d/%d/%d\tSLF4J:%d/%d/%d",
+                        (int) julStats.mean(), (int) julStats.max(), (int) julStats.sampleStandardDeviation(),
+                        (int) jclStats.mean(), (int) jclStats.max(), (int) jclStats.sampleStandardDeviation(),
+                        (int) log4jStats.mean(), (int) log4jStats.max(), (int) log4jStats.sampleStandardDeviation(),
+                        (int) slf4jStats.mean(), (int) slf4jStats.max(), (int) slf4jStats.sampleStandardDeviation()));
+            } catch (Exception e4) {
+                e4.printStackTrace();
+            } finally {
+                statsLock.readLock().unlock();
+            }
+        }, 3, 3, TimeUnit.SECONDS);
+
         try {
             CountDownLatch latch = new CountDownLatch(numberOfThreads);
-            ExecutorService es = Executors.newCachedThreadPool();
+            ExecutorService es = newCachedThreadPool();
             for (int t = 0; t < numberOfThreads; t++) {
                 es.submit(new LogRunnable(t, numberOflogs, e, latch));
                 Thread.sleep(random.nextInt(MAX_THREAD_BACKOFF_IN_MS));
             }
             latch.await();
+            scheduledExecutor.shutdown();
         } catch (Exception e3) {
             throw new RuntimeException("Logging failed", e3);
         }
     }
 
+    void addStats(long jul, long log4j, long jcl, long slf4j) {
+        try {
+            statsLock.writeLock().lock();
+            julStats.add(jul);
+            log4jStats.add(log4j);
+            jclStats.add(jcl);
+            slf4jStats.add(slf4j);
+        } finally {
+            statsLock.writeLock().unlock();
+        }
+    }
+
+    synchronized void addJulStats(long mics) {
+        julStats.add(mics);
+    }
 
     class LogRunnable implements Runnable {
 
@@ -66,24 +115,39 @@ public class Log {
         @Override
         public void run() {
             for (int i = 0; i < numberOflogs; i++) {
-                jul.info(format("jul-info-%s-%s", t, i));
-                jul.log(Level.WARNING, format("jul-warn-", t, i), e);
-                jul.severe(format("jul-severe-" + i));
 
-                log4j.info(format("log4j-info-", t, i));
-                log4j.warn(format("log4j-warn-", t, i), e);
-                log4j.error(format("log4j-error-", t, i));
+                long startTime = nanoTime();
+                jul.info(format("jul-info-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE));
+                jul.log(Level.WARNING, format("jul-warn-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE), e);
+                jul.severe(format("jul-severe-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE));
+                long julMics = (nanoTime() - startTime) / 1000;
 
-                jcl.info(format("jcl-info-", t, i));
-                jcl.warn(format("jc-warn-", t, i));
-                jcl.error(format("jcl-error-", t, i), e);
+                startTime = nanoTime();
+                log4j.info(format("log4j-info-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE));
+                log4j.warn(format("log4j-warn-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE), e);
+                log4j.error(format("log4j-error-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE));
+                long log4jMics = (nanoTime() - startTime) / 1000;
 
-                slf4j.info(format("slf4j-info-", t, i));
-                slf4j.warn(format("sfl4j-warn-", t, i), e);
-                slf4j.error(format("slf4j-error-", t, i));
+                startTime = nanoTime();
+                jcl.info(format("jcl-info-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE));
+                jcl.warn(format("jc-warn-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE));
+                jcl.error(format("jcl-error-%s-%s-%s", t, i, RANDOM_LOG_MESSAGE), e);
+                long jclMics = (nanoTime() - startTime) / 1000;
 
-                if (i % 100 == 0) {
-                    System.out.println(format("Thread %s, Iteration %s", t, i));
+                startTime = nanoTime();
+                slf4j.info("slf4j-info-{}-{}-{}", t, i, RANDOM_LOG_MESSAGE);
+                slf4j.warn("sfl4j-warn-{}-{}-{}", t, i, RANDOM_LOG_MESSAGE, e);
+                slf4j.error("slf4j-error-{}-{}-{}", t, i, RANDOM_LOG_MESSAGE);
+                long slf4jMics = (nanoTime() - startTime) / 1000;
+
+                try {
+                    statsLock.writeLock().lock();
+                    julStats.add(julMics);
+                    log4jStats.add(log4jMics);
+                    jclStats.add(jclMics);
+                    slf4jStats.add(slf4jMics);
+                } finally {
+                    statsLock.writeLock().unlock();
                 }
 
                 try {
--- src/test/java/uk/gleissner/log/LogTest.java
+++ src/test/java/uk/gleissner/log/LogTest.java
@@ -1,11 +1,14 @@
 package uk.gleissner.log;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.File;
 
-import static org.junit.Assert.assertTrue;
+import static org.apache.commons.io.FileUtils.cleanDirectory;
+import static org.apache.commons.io.FileUtils.listFiles;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
 
 public class LogTest {
 
@@ -13,15 +16,21 @@ public class LogTest {
 
     static {
         try {
-            FileUtils.cleanDirectory(LOG_DIR);
-            assertTrue(FileUtils.listFiles(LOG_DIR, new String[]{".log"}, true).isEmpty());
+            cleanDirectory(LOG_DIR);
+            assertThat(getNumberOfLogFiles(), is(0));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+
     @Test
-    public void canLog() {
+    public void canLog() throws InterruptedException {
         new Log(2, 10);
+        assertThat(getNumberOfLogFiles(), is(greaterThan(0)));
+    }
+
+    private static int getNumberOfLogFiles() {
+        return listFiles(LOG_DIR, new String[]{"log"}, true).size();
     }
 }
