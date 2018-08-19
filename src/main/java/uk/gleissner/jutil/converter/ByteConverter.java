5813fc8e5035c601bc05f76a7ca8c5370d29eed8
Christian Gleissner <a-cgleissner@expedia.com>
2018-08-19 19:06:29 +0100

    License comment

--- src/main/java/uk/gleissner/jutil/converter/ByteConverter.java
+++ src/main/java/uk/gleissner/jutil/converter/ByteConverter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2018 Christian Gleissner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gleissner.jutil.converter;
 
 import com.google.protobuf.Message;
@@ -9,13 +24,13 @@ public class ByteConverter {
     private final static char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
     public static String toHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
+        char[] hex = new char[bytes.length * 2];
         for (int i = 0; i < bytes.length; i++) {
             int b = bytes[i] & 0xFF;
-            hexChars[i * 2] = HEX_ARRAY[b >>> 4];
-            hexChars[i * 2 + 1] = HEX_ARRAY[b & 0x0F];
+            hex[i * 2] = HEX_ARRAY[b >>> 4];
+            hex[i * 2 + 1] = HEX_ARRAY[b & 0x0F];
         }
-        return new String(hexChars);
+        return new String(hex);
     }
 
     public static String toSpacedHex(Message msg) {
--- src/main/java/uk/gleissner/jutil/protobuf/ProtobufFieldPartitioner.java
+++ src/main/java/uk/gleissner/jutil/protobuf/ProtobufFieldPartitioner.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2018 Christian Gleissner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gleissner.jutil.protobuf;
 
 import com.google.protobuf.Descriptors;
@@ -20,7 +35,7 @@ import static java.util.Optional.empty;
 public class ProtobufFieldPartitioner {
 
     public static <M extends Message, N> Collection<M> partitition(M msg, Descriptors.FieldDescriptor field,
-                                                                    long maxMsgSizeInBytes) {
+                                                                   long maxMsgSizeInBytes) {
         FieldPartitioner.ObjectBuilder<M> objectBuilder = () -> (M) msg.toBuilder().clearField(field).build();
         FieldPartitioner.FieldAdder<M, N> fieldAdder = (m, n) -> {
             Optional<M> optionalMessageWithField = empty();
