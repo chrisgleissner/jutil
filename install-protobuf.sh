06f9e8c996c754216047141fafeca8eb26ca2501
Christian Gleissner <a-cgleissner@expedia.com>
2018-08-19 13:00:12 +0100

    Extended travis for protobuf

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,5 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 set -ex
-wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
-tar -xzvf protobuf-2.4.1.tar.gz
-cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
+
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+cd /home/travis
+
+case "$PROTOBUF_VERSION" in
+2*)
+    basename=protobuf-$PROTOBUF_VERSION
+    wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz
+    tar xzf $basename.tar.gz
+    cd protobuf-$PROTOBUF_VERSION
+    ./configure --prefix=/home/travis && make -j2 && make install
+    ;;
+3*)
+    basename=protoc-$PROTOBUF_VERSION-linux-x86_64
+    wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.zip
+    unzip $basename.zip
+    ;;
+*)
+    die "unknown protobuf version: $PROTOBUF_VERSION"
+    ;;
+esac
