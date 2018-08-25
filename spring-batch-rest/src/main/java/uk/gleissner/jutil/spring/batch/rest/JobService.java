78d5be93aaad1670db6424dfd2daa4b9d7a4d274
Christian Gleissner <chrisgleissner@gmail.com>
2018-08-25 12:47:01 +0100

    Started wiring job explorer

--- spring-batch-rest/src/main/java/uk/gleissner/jutil/spring/batch/rest/JobService.java
+++ spring-batch-rest/src/main/java/uk/gleissner/jutil/spring/batch/rest/JobService.java
@@ -1,16 +1,27 @@
 package uk.gleissner.jutil.spring.batch.rest;
 
 import com.google.common.collect.Lists;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gleissner.jutil.spring.batch.rest.domain.JobExecution;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class JobService {
 
+    @Autowired
+    private JobExplorer jobExplorer;
+
     public List<JobExecution> getJobExecutions(Optional<JobExecution.Status> status) {
+        int startRow = 0;
+        int count = 1000;
+//        jobExplorer.getJobNames().stream()
+//                .flatMap(jn -> jobExplorer.getJobInstances(jn, startRow, count))
+//                .flatMap(jobExplorer::getJobExecutions).collect(Collectors.toList());
         // TODO
         return Lists.newArrayList();
     }
--- spring-batch-rest/src/main/java/uk/gleissner/jutil/spring/batch/rest/SpringBatchRestApplication.java
+++ spring-batch-rest/src/main/java/uk/gleissner/jutil/spring/batch/rest/SpringBatchRestApplication.java
@@ -1,9 +1,11 @@
 package uk.gleissner.jutil.spring.batch.rest;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchRestApplication {
 
     public static void main(String[] args) {
