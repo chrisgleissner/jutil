213f4aeb1d8f8061207ddc4baa77a255e041d15f
Christian Gleissner <chrisgleissner@gmail.com>
2018-08-26 20:18:01 +0100

    Removed unnecessary code

--- spring-batch/src/test/java/uk/gleissner/jutil/spring/batch/springboot/QuartzConfig.java
+++ spring-batch/src/test/java/uk/gleissner/jutil/spring/batch/springboot/QuartzConfig.java
@@ -34,15 +34,11 @@ public class QuartzConfig {
     public JobDetailFactoryBean jobDetailFactoryBean() {
         JobDetailFactoryBean factory = new JobDetailFactoryBean();
         factory.setJobClass(QuartzJobLauncher.class);
-
         Map map = new HashMap();
         map.put("jobName", "importUserJob");
         map.put("jobLauncher", jobLauncher);
         map.put("jobLocator", jobLocator);
         factory.setJobDataAsMap(map);
-
-        factory.setGroup("group");
-        factory.setName("job");
         return factory;
     }
 
@@ -50,15 +46,14 @@ public class QuartzConfig {
     public CronTriggerFactoryBean cronTriggerFactoryBean() {
         CronTriggerFactoryBean triggerFactory = new CronTriggerFactoryBean();
         triggerFactory.setJobDetail(jobDetailFactoryBean().getObject());
-        triggerFactory.setStartDelay(0);
-        triggerFactory.setName("trigger");
-        triggerFactory.setGroup("group");
         triggerFactory.setCronExpression("0/1 * * * * ?");
         return triggerFactory;
     }
 
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
+        // TODO Use builder instead
+        // CronTrigger cronTrigger = TriggerBuilder.newTrigger().withSchedule(cronSchedule("0/1 * * * * ?")).forJob("importUserJob").build();
         SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
         scheduler.setTriggers(cronTriggerFactoryBean().getObject());
         return scheduler;
