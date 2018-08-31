a8782560e5391032a71eb5e6130acb36777dee51
Christian Gleissner <chrisgleissner@gmail.com>
2018-08-31 22:14:00 +0100

    Fixed compilation issue

--- spring-batch-rest/src/main/java/uk/gleissner/jutil/spring/batch/rest/SpringBatchRestApplication.java
+++ spring-batch-rest/src/main/java/uk/gleissner/jutil/spring/batch/rest/SpringBatchRestApplication.java
@@ -1,12 +1,10 @@
 package uk.gleissner.jutil.spring.batch.rest;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer
 @EnableBatchProcessing
 public class SpringBatchRestApplication {
 
